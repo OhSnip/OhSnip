542ffe840fbc4db413b414597b73c22b034f38cc
Patrick <kaorti@gmail.com>
2017-11-15 15:47:29 -0800

    migrations work, models seem to be correct, need testing.

--- OhSnip/Data/Migrations/20171115213237_InitialCreate.Designer.cs
+++ OhSnip/Data/Migrations/20171115213237_InitialCreate.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace OhSnip.Data.Migrations
 {
     [DbContext(typeof(OhSnipContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171115213237_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
