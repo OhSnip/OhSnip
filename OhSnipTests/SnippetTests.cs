99bc56510e9d88bb5ddfb5cf59974e5b29029d96
Ari <ariwolpin@gmail.com>
2017-11-17 10:21:14 -0800

    added test framework

---
+++ OhSnipTests/SnippetTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OhSnip;
+
+namespace OhSnipTests
+{
+    [TestClass]
+    public class SnippetTests
+    {
+
+        //[TestMethod]
+        //public void AddSnippet_UpdatesList()
+        //{
+        //    //arrange
+        //    int SnippetId = 100;
+        //    string Title = "html";
+        //    string Description = "easy one";
+        //    string Language = "HTML";
+        //    string Code = "<html></html>";
+        //    string ApplicationUserId = "1";
+        //    Snippet snip = new Snippet(SnippetId, Title, Description, Language, Code, ApplicationUserId)
+
+        //    //act
+        //    //add method with above
+
+        //    //assert
+        //    //
+
+        //}
+
+    }
+    }
+}
