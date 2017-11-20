ded7de22d675ded408c00cad3a00ec9d37d5ef76
Patrick <kaorti@gmail.com>
2017-11-20 14:52:42 -0800

    code copy button

---
+++ OhSnip/wwwroot/js/CodeCopy.js
@@ -0,0 +1,19 @@
+﻿(function (module) {
+    var clipboard = new Clipboard('.btn', {
+        target: function (trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+    clipboard.on('success', function (e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function (e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
+})(window);
